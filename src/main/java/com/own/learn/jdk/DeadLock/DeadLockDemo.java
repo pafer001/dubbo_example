63b860e985a34fffafc783f2c69506da6fd51a3f
pafer <zhenyawang@sohu-inc.com>
2018-02-07 19:27:41 +0800

    提交代码

---
+++ src/main/java/com/own/learn/jdk/DeadLock/DeadLockDemo.java
@@ -0,0 +1,53 @@
+package com.own.learn.jdk.DeadLock;
+
+/**
+ * @author wangzhenya
+ */
+public class DeadLockDemo {
+
+    private static String A = "A";
+    private static String B = "B";
+
+
+    public static void main(String[] args) {
+
+        new DeadLockDemo().deadLock();
+    }
+    private void deadLock() {
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                synchronized (A) {
+
+                    try {
+
+                        Thread.currentThread().sleep(20000000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                synchronized (B) {
+                        System.out.println("1");
+                }
+            }
+        });
+
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (B) {
+                    synchronized (A) {
+                        System.out.println("2");
+                    }
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}

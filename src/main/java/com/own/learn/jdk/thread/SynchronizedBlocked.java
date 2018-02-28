1ff974cf7adf6a2dc870d2c62c6c9713b298c062
pafer <zhenyawang@sohu-inc.com>
2018-02-28 15:37:00 +0800

    提交代码

---
+++ src/main/java/com/own/learn/jdk/thread/SynchronizedBlocked.java
@@ -0,0 +1,40 @@
+package com.own.learn.jdk.thread;
+
+public class SynchronizedBlocked {
+
+
+    static Integer sigal = 1;
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (sigal) {
+
+                    try {
+//                        Thread.sleep(1000000);
+                        sigal.wait();
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        }, "thread-1").start();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (sigal) {
+
+                    try {
+                        System.out.println("111");
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        }, "thread-2").start();
+
+
+    }
+}

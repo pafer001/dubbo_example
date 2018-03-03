921a4e1b2d0b9284abef66b7e7e7cd576da490d7
pafer <zhenyawang@sohu-inc.com>
2018-03-03 11:13:02 +0800

    修改代码

---
+++ src/main/java/com/own/learn/jdk/thread/InterruptTest.java
@@ -0,0 +1,40 @@
+package com.own.learn.jdk.thread;
+
+/**
+ * @author pafer
+ */
+public class InterruptTest {
+
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            int i = 0;
+
+            try {
+                while (i < 1000) {
+                    Thread.sleep(500);
+                    System.out.println(i++);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+
+    public static void main(String[] args) {
+
+        InterruptTest interruptTest = new InterruptTest();
+        Thread thread = new Thread(interruptTest.runnable);
+        thread.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        thread.interrupt();
+    }
+}

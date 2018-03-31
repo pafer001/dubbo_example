698ec996f771ecf01465ea324dcd1f7f6472fd58
pafer <zhenyawang@sohu-inc.com>
2018-03-31 19:37:42 +0800

    embedded exception

---
+++ src/main/java/com/own/learn/netty/_9/FrameChunkDecoder.java
@@ -0,0 +1,32 @@
+package com.own.learn.netty._9;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.TooLongFrameException;
+
+import java.util.List;
+
+public class FrameChunkDecoder extends ByteToMessageDecoder{
+
+
+    private final int maxFrameSize;
+
+    public FrameChunkDecoder(int maxFrameSize) {
+        this.maxFrameSize = maxFrameSize;
+    }
+
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+
+        int i = in.readableBytes();
+        if (i > maxFrameSize) {
+            in.clear();
+            throw new TooLongFrameException();
+        }
+
+        ByteBuf byteBuf = in.readBytes(i);
+        out.add(byteBuf);
+    }
+}

270230c64f17048531b1779dec59973bed105a9a
pafer <zhenyawang@sohu-inc.com>
2018-04-04 15:04:29 +0800

    修改websocket

---
+++ src/main/java/com/own/learn/netty/_12/TextWebSocketFrameHandler.java
@@ -0,0 +1,22 @@
+package com.own.learn.netty._12;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    private final ChannelGroup group;
+
+    public TextWebSocketFrameHandler(ChannelGroup group) {
+        this.group = group;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+
+        group.writeAndFlush(msg.retain());
+    }
+}

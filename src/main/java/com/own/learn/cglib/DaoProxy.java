00f925b92fd071a9dedbd584b45e7f2df089e716
pafer <zhenyawang@sohu-inc.com>
2018-08-02 14:40:25 +0800

    jvm内存溢出

---
+++ src/main/java/com/own/learn/cglib/DaoProxy.java
@@ -0,0 +1,20 @@
+package com.own.learn.cglib;
+
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class DaoProxy implements MethodInterceptor {
+
+    @Override
+    public Object intercept(Object object, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+
+
+        System.out.println("Before Method Invoke");
+        methodProxy.invokeSuper(object, objects);
+        System.out.println("After Method Invoke");
+
+        return object;
+    }
+}

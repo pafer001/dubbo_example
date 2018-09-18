9c071f1cbc1eb2e2d6231d92e7427a2f8b868281
pafer <zhenyawang@sohu-inc.com>
2018-09-18 16:55:11 +0800

    练习dump sort

---
+++ src/main/java/com/own/algorithm/sort/exercise/BinaryHeapSort1.java
@@ -0,0 +1,50 @@
+package com.own.algorithm.sort.exercise;
+
+import com.own.algorithm.sort.BaseSort;
+import com.own.algorithm.sort.BinaryHeapSort;
+
+public class BinaryHeapSort1 extends BaseSort {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 7, 3, 5, 6, 0};
+
+        new BinaryHeapSort1().sort(array);
+        print(array);
+    }
+
+    public void sort(int [] array) {
+        int len = array.length -1;
+
+        for (int i=0; i < len/ 2 -1; i++) {
+            sink(array, i, len);
+        }
+
+        for (int i = len -1; i>= 0; i--) {
+            swap(array, i, 0);
+            sink(array, 0, len);
+        }
+    }
+    public void sink(int[] array, int i, int len) {
+
+        int left, right;
+        while (2 * i + 1 < len ) {
+            left = 2* i +1;
+            right = 2* i +2;
+
+            int j = left;
+            if (array[left] < array[right]) {
+                j = right;
+            }
+
+            if (array[j] > array[i]) {
+                swap(array, i, j);
+            } else {
+                break;
+            }
+
+            i= j;
+
+        }
+    }
+}

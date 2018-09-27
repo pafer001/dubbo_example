2c08863260d1e6b2aaf0b241622fd902607f5a4d
pafer <769568569@qq.com>
2018-09-27 23:04:42 +0800

    leetcode

---
+++ src/main/java/com/own/algorithm/leetcode/FourSum.java
@@ -0,0 +1,37 @@
+package com.own.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        List<List<Integer>> rets = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return new ArrayList<>();
+        }
+
+        for (int i =0; i < nums.length; i++) {
+
+            for (int j = i+1; j < nums.length; j++) {
+
+                for (int m = j + 1; m < nums.length; m++) {
+                    for (int n = m +1; n < nums.length; n ++) {
+
+                        if (nums[i] + nums[j] + nums[m] + nums[n] == target) {
+                            ArrayList<Integer> objects = new ArrayList<>();
+                            objects.add(nums[i]);
+                            objects.add(nums[j]);
+                            objects.add(nums[m]);
+                            objects.add(nums[n]);
+
+                            rets.add(objects);
+                        }
+                    }
+                }
+            }
+        }
+        return rets;
+    }
+}
---
+++ src/main/java/com/own/algorithm/leetcode/LetterCombinations.java
@@ -0,0 +1,18 @@
+//package com.own.algorithm.leetcode;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//public class LetterCombinations {
+//
+//
+//    public List<String> letterCombinations(String digits) {
+//
+//        char[] digitArray = digits.toCharArray();
+//
+//        for (char digit : digitArray) {
+//
+//
+//        }
+//    }
+//}

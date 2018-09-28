cb9485bd9cb878343a075df4e4551e7bf8b142eb
pafer <769568569@qq.com>
2018-09-28 22:53:52 +0800

    leetcode

---
+++ src/main/java/com/own/algorithm/leetcode/BinarySearch.java
@@ -0,0 +1,36 @@
+package com.own.algorithm.leetcode;
+
+/**
+ * 704. Binary Search
+ */
+public class BinarySearch {
+
+//    public static void main(String[] args) {
+//
+//        int nums[]
+//        new BinarySearch().search()
+//    }
+
+    public int search(int[] nums, int target) {
+
+        if (nums == null || nums.length == 0) {
+            return  -1;
+        }
+
+        return search(nums, 0, nums.length -1, target);
+    }
+
+    public int search(int[] nums, int low, int high, int target) {
+        if (low > high) {
+            return -1;
+        }
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return search(nums, low, mid - 1, target);
+        } else {
+            return search(nums, mid + 1, high, target);
+        }
+    }
+}
---
+++ src/main/java/com/own/algorithm/leetcode/UncommonFromSentences.java
@@ -0,0 +1,50 @@
+package com.own.algorithm.leetcode;
+
+import java.util.*;
+
+public class UncommonFromSentences {
+
+    public static void main(String[] args) {
+
+        String[] strings = new UncommonFromSentences().uncommonFromSentences("apple apple",
+                "banana");
+        System.out.println(strings);
+    }
+
+    public String[] uncommonFromSentences(String A, String B) {
+
+        Map<String, Integer> wodMap = new HashMap<>();
+        wordMap(A, wodMap);
+        wordMap(B, wodMap);
+
+
+
+        List<String> returnList = new ArrayList<>();
+
+        String[] aWordArray = A.split(" ");
+        for (String aWord : aWordArray) {
+            if (wodMap.getOrDefault(aWord, 1) == 1) {
+                returnList.add(aWord);
+            }
+        }
+
+        String[] bWordArray = B.split(" ");
+        for (String bWord : bWordArray) {
+            if (wodMap.getOrDefault(bWord, 1) == 1) {
+                returnList.add(bWord);
+            }
+        }
+
+        return returnList.toArray(new String[returnList.size()]);
+
+    }
+
+    private void wordMap(String A, Map<String, Integer> wodMap) {
+        String[] wordArray = A.split(" ");
+        for (String word : wordArray) {
+            Integer orDefault = wodMap.getOrDefault(word, 0);
+            wodMap.put(word, orDefault+1);
+        }
+    }
+
+}
